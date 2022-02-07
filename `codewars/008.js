be2e81a8bedce33234707c1410b14a94b9e94614
sockromant <s.s.chiglintsev@gmail.com>
2022-02-07 17:05:52 +0500

    008

---
+++ `codewars/008.js
@@ -0,0 +1,14 @@
+function getCount(str) {
+    var vowelsCount = 0;
+    let gl = ["a","e","i","o","u"];
+    for (let i=0;i<str.length;i++) {
+        for (let j=0;j<gl.length;j++) {
+            if (gl[j]==str[i]) {
+                vowelsCount++;
+            }
+        }
+    }
+    // enter your majic here
+
+    return vowelsCount;
+}
