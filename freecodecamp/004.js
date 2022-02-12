0cf8da5d0629f6a39acbacc468409cbd9a793aba
sockromant <s.s.chiglintsev@gmail.com>
2022-02-12 15:20:40 +0500

    008

---
+++ freecodecamp/004.js
@@ -0,0 +1,11 @@
+Code	Output
+\'	single quote
+\"	double quote
+\\	backslash
+\n	newline
+\r	carriage return
+\t	tab
+\b	word boundary
+\f	form feed
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
